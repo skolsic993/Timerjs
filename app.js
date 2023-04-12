39acf8eda97b76e6356dcaaffbaee884abdd7a18
Miloš Stojsavljević <skolsic993@gmail.com>
2023-04-12 15:56:23 +0200

    Add tick, start and stop functionality

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 //Timer class
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startButton.addEventListener("click", this.start);
     this.pauseButton.addEventListener("click", this.pause);
   }
 
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
+
     this.tick();
     this.interval = setInterval(this.tick, 1000);
-    clearInterval(timer);
   };
 
   pause = () => {
@@ -20,7 +37,13 @@ class Timer {
   };
 
   tick = () => {
-    console.log("tick");
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) this.onComplete();
+    } else {
+      this.timeRemaining -= 1;
+      if (this.onTick) this.onTick();
+    }
   };
 }
 
@@ -28,4 +51,14 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer ticked down!");
+  },
+  onComplete() {
+    console.log("Timer completed!");
+  },
+});
