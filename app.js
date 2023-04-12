7cf1e1a18f96eb9605f408d01b0fc26ca17d82c6
Miloš Stojsavljević <skolsic993@gmail.com>
2023-04-12 15:27:23 +0200

    Add timer class and basic functionality

---
+++ app.js
@@ -0,0 +1,31 @@
+//Timer class
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+    clearInterval(timer);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    console.log("tick");
+  };
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
